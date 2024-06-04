1c058c54336cdd887850c5792009c42c53d56850
Dhruv Agarwal <92082181+Dhruv7055@users.noreply.github.com>
2024-06-04 18:41:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "608344b2b7b3448eb5479c7e992463aa";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
